161289bc84dc9e8993b2cefebe2e35cd9a29041d
gilyong <gilll99@naver.com>
2025-07-26 17:21:56 +0900

    계산기 덧셈

---
+++ source/calculator_plus.py
@@ -0,0 +1,40 @@
+#print 함수는 프린터 함수의 인자{()안의 수}에서 터미널로 출력하는 함수이다.
+# print(1)
+
+#input 함수는()안에 '원하는단어' 설정 후 =ㅇ에 받을 값 설정/ 터미널에서 입력을 받는 함수이다.
+# i = input('테스트')
+# print(i)
+
+# k = input(i)
+# print(k)
+
+#int 함수는 입력된 값을 정수로 변환하는 함수이다.
+# a=int(input('첫번째 숫자를 입력하세요: '))
+# b=int(input('두번째 숫자를 입력하세요: '))
+# c=a+b
+# print('The sum of', a, 'and', b, 'is', c)
+
+#list 함수는 입력된 값을 리스트로 변환하는 함수이다. - list는 문자열만 변환 가능하고 숫자 변환은 불가능하다.
+#split 함수는 자동으로 list로 변환해준다.
+#split 함수는("")안의 기준으로 문자열을 분리하는 함수이다.
+# t=input('Enter your name: ').split(" ")
+# print(t)
+
+#map 함수는 문자열을 int를 사용해 숫자로 변환가능한 함수이다.(한정x)/ list를 사용해 리스트로 재변환 해야한다
+# t=input('Enter your name: ').split(" ")
+# a= list(map(int, t))
+# print(a)
+
+#for 반복문은 리스트의 길이만큼 반복하는 함수이다.
+#result 변수에 0을 대입하고, for 반복문을 사용해 a의 길이만큼 반복하며 i를 result에 더하는 함수이다.
+# t=input('Enter your name: ').split(" ")
+# a= list(map(int, t))
+# result = 0
+# for i in a:   
+#     result += i
+# print(result)      
+
+# =sum(list)로 리스트 합 퉁치기 가능
+# list1 = [1, 2, 3, 4, 5]
+# a=sum(list1)
+# print(a)
